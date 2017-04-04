231a71b83dc5b26739ad62d9407bce6fb37b1ca4
Ricardo Pimienta <rpimienta@itexico.net>
2017-04-04 10:42:43 -0500

    continuation of course

--- section 5/inherits example/app.js
+++ section 5/inherits example/app.js
@@ -1,21 +1,10 @@
-var EventEmitter = require('events');
-var util = require('util');
-
-function Greetr () {
-    this.greeting = "Hello World";
-}; 
-
-util.inherits(Greetr, EventEmitter);
-
-Greetr.prototype.greet = function (data) {
-     console.log(this.greeting+ ": " + data);
-     this.emit('greet', data);
- }
+'use strict';
+var Greetr = require('./greetr');
 
  var greetr1 = new Greetr();
 
  greetr1.on('greet', function (data) {
-     console.log('Someone greets!: '+ data);
+     console.log('Someone is greeting!: '+ data);
  });
 
  greetr1.greet('Ricardo');
